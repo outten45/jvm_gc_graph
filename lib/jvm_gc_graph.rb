4a8779b038adea5ff3287d681146ec41b94bcff0
Richard Outten (mbp) <outtenr@duke.edu>
2010-12-26 22:19:58 -0500

    include graph of gc times

--- lib/jvm_gc_graph.rb
+++ lib/jvm_gc_graph.rb
@@ -6,8 +6,8 @@ module JvmGcGraph
   class Runner
 
     class << self
-      def run(file, output)
-        pml = JvmGcGraph::ParseMemoryLog.new(file)
+      def run(opts)
+        pml = JvmGcGraph::ParseMemoryLog.new(opts)
         pml.json
         output(pml)
       end
@@ -27,26 +27,39 @@ module JvmGcGraph
     FULL_GC_REGEX  = /^(([0-9]|\.)*):\s\[(Full GC\s([0-9]*)K->([0-9]*)K\(([0-9]*)K\)),\s(([0-9]|\.)*).*\]/
     ANY_GC_REGEX   = /^(([0-9]|\.)*):\s\[((Full )?GC\s([0-9]*)K->([0-9]*)K\(([0-9]*)K\)),\s(([0-9]|\.)*).*\]/
 
-    attr_accessor :lines, :start_time, :data
+    attr_accessor :lines, :start_time, :data, :gc_data
 
-    def initialize(log_file)
-      File.open(log_file, "r") do |f|
+    def initialize(opts)
+      File.open(opts[:file], "r") do |f|
         @lines = f.readlines
       end
-      puts @lines.last
       if m = @lines.last.match(ANY_GC_REGEX)
         @start_time = Time.now.to_i - m[0].to_f
       end
-      puts @start_time
       @data = []
+      @gc_data = []
+      self.class.send(:define_method, :title) {opts[:title]}
     end
 
     def parse_lines
       lines.each do |line|
         if m = line.match(GC_REGEX)
-          @data << [((@start_time + m[0].to_i)*1000), m[4], nil,nil]
+          @data << [
+                    ((@start_time + m[0].to_i)*1000),
+                    m[4].to_f, nil,nil,
+                    m[5].to_f, nil,nil,
+                    m[6].to_f, nil,nil
+                   ]
+          @gc_data << [((@start_time + m[0].to_i)*1000), (m[7].to_f*1000), nil,nil]
         elsif m = line.match(FULL_GC_REGEX)
-          puts "must be FULL: #{line}"
+          # puts "must be FULL: #{line}"
+          @data << [
+                    ((@start_time + m[0].to_i)*1000),
+                    m[4].to_f, nil, nil,
+                    m[5].to_f, nil,nil,
+                    m[6].to_f, nil,nil
+                   ]
+          @gc_data << [((@start_time + m[0].to_i)*1000), (m[7].to_f*1000), nil,nil]
         end
       end
     end
@@ -55,7 +68,10 @@ module JvmGcGraph
       parse_lines
       @data
     end
-    
+
+    def get_binding
+      binding
+    end
   end
 
 end
